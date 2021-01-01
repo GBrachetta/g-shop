f27d164aa1db70019448b200d4659db682bbf21d
Guillermo Brachetta <brachetta@me.com>
2021-01-01 02:46:53 +0100

    Add react router implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,21 @@ import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProducScreen from './screens/ProducScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProducScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
